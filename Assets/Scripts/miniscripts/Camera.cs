fb62c1db9e0e13864361b72db1a18b954ab9cff8
CTPOKA <gnedenkov777daniil@gamil.com>
2019-12-26 21:28:00 +0700

    Revert "New"
    This reverts commit 87bcf6688c1ecc70b43d15bace800c0339945f1e.

--- Assets/Scripts/miniscripts/Camera.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class Camera : MonoBehaviour
-{
-    private void Start()
-    {
-        transform.position = new Vector2(Editor.E.X * Editor.cellsizeX / 2, (Editor.E.Y + 1) * Editor.cellsizeY / 2);
-    }
-}
